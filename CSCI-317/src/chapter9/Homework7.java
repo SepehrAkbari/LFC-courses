6300ac5c3550a504f8f169095fc5f65067622790
Sepehr Akbari <isepehrakbari@gmail.com>
2024-11-09 12:02:01 -0600

    317: hw7

--- CSCI-317/src/chapter9/Homework7.java
+++ CSCI-317/src/chapter9/Homework7.java
@@ -1,10 +1,11 @@
 package chapter9;
 
-public class MyHashTable {
+// Code is based on the MyHashTable class given in class, insert() method was modified and rehash() added
+class MyHashTable2 {
 	private long[] data;
 	private int count;
 	
-	public MyHashTable(int size) {
+	public MyHashTable2(int size) {
 		/* Should we use size directly? More on this later... */
 		int s = size;
 		while(!isPrime(s++));
@@ -28,24 +29,24 @@ public class MyHashTable {
 	}
 	
 	public boolean insert(long key) {
-		if(!isFull()) {
-			int index = hash(key);
-			if(data[index] == 0)//No data at that spot
-				data[index] = key;
-			else {
-				while(data[index]>0) {//Keep probing for empty spot
-					index++;
-					if(index==data.length)//Go back to the beginning
-						index = 0;
-				}
-				data[index] = key;
-			}
-			count++;
-			return true;
-		}
-		else
-			return false;
-	}
+        if ((double) count / data.length > 0.5) {
+            rehash();
+        }
+
+        int index = hash(key);
+        if (data[index] == 0)
+            data[index] = key;
+        else {
+            while (data[index] > 0) {
+                index++;
+                if (index == data.length)
+                    index = 0;
+            }
+            data[index] = key;
+        }
+        count++;
+        return true;
+    }
 	
 	public boolean remove(long key) {
 		int index = hash(key);
@@ -67,7 +68,6 @@ public class MyHashTable {
 			} 
 			return false;
 		}
-		
 	}
 	
 	public boolean search(long key) {
@@ -102,4 +102,35 @@ public class MyHashTable {
 		for(int i=0; i<data.length; i++)
 			System.out.println(i+":" + data[i]);
 	}
+
+    // REHASH() METHOD
+    public void rehash() {
+        int newSize = data.length * 2;
+        while (!isPrime(newSize)) newSize++;
+    
+        long[] oldData = data;
+        data = new long[newSize];
+        count = 0;
+    
+        for (long key : oldData) {
+            if (key > 0) { 
+                insert(key);
+            }
+        }
+        System.out.println("Rehashed table to size " + newSize);
+    }
+}
+
+public class Homework7 {
+    // DRIVER CLASS
+    public static void main(String[] args) {
+        MyHashTable2 table = new MyHashTable2(5);
+
+        System.out.println("Inserting elements:");
+        for (long i = 1; i <= 10; i++) {
+            table.insert(i);
+            table.display();
+            System.out.println("------");
+        }
+    }
 }
