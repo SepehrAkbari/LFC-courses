7b97d9ebed90e06c66225a407689e00f8d894767
Sepehr Akbari <isepehrakbari@gmail.com>
2024-11-15 08:48:08 -0600

    317: remove() update

--- CSCI-317/src/chapter8/MyBinarySearchTree.java
+++ CSCI-317/src/chapter8/MyBinarySearchTree.java
@@ -3,131 +3,128 @@ package chapter8;
 public class MyBinarySearchTree {
 
 	private MyNode root;
+	private int count;
+
 	public MyBinarySearchTree() {
 		root = null;
+		count = 0;
 	}
 
 	public MyBinarySearchTree(int data) {
 		root = new MyNode(data);
+		count = 1;
 	}
-
-	public MyBinarySearchTree(MyNode root) {
-		this.root = root;
-	}
-
-
+	
 	public void insert(int key) {
-		if(key < root.getData()) {
-			if (root.getLeft()==null) {
-				root.setLeft(new MyNode(key));
-			}	
-			else {	
-				MyBinarySearchTree current = new MyBinarySearchTree(root.getLeft());
-				current.insert(key);
-			}
-		}
-		else if(key > root.getData()){
-			if (root.getRight()==null) {
-				root.setRight(new MyNode(key));
-			}	
-			else {
-				MyBinarySearchTree current = new MyBinarySearchTree(root.getRight());
-				current.insert(key);
-			}
-		}
-
-		else // if key == data
-			root.increaseFrequency();
+		root = insertRec(root, key);
 	}
 
-    /*
-	/// This method is still incomplete
-	public boolean remove(int key) {
-		// First, find the item
-		if(search(key)==false)
-			return false;
-		// Check if root is the only node and needs to be deleted
-		if(root.getData()==key && 
-				root.getLeft()==null && root.getRight()==null) {
-			if(root.getFrequency()>1) {
-				root.decreaseFrequency();
-				return true;
-			}
-			else {
-				root = null;
-				return true;
-			}
+	private MyNode insertRec(MyNode root, int key) {
+		if(root==null) {
+			root = new MyNode(key);
+			return root;
 		}
 		
-		// Now decide if the key is left or right
-		if(key < data) {
-			MyBinarySearchTree current = this;
-			while(current.left > key) {
-				current = current.left;
-			}
-
+		if(root.getData()>key) {
+			//insert on left subtree
+			root.setLeft(insertRec(root.getLeft(), key));
 		}
-		else if(key > data) {
-
+		else if(root.getData() < key) {
+			//insert on right subtree
+			root.setRight(insertRec(root.getRight(), key));
 		}
-		else {//if key == data
+		else // if key == data
+			root.increaseFrequency();
+		count++;
+		return root;
+	}
 
-			// Case 0: item has a frequency > 1
-			if(frequency>1) {
-				frequency--;
-				return true;
-			}
-			// Case 1: item is a leaf
-			if()
-
-				// Case 2: item has one child
-				// Case 3: item has two children
-		}
+	public void remove(int key) {
+		root = removeRec(root, key);
 
 	}
-    */
-
-
-	public boolean search(int key) {
+	
+	public MyNode removeRec(MyNode root, int key) {
+		if(root==null)
+			return root;
+		
 		if(key < root.getData()) {
-			if (root.getLeft()==null)
-				return false;
-			else {
-				MyBinarySearchTree current = new MyBinarySearchTree(root.getLeft());	
-				return current.search(key);
-			}
+			// remove from left subtree
+			root.setLeft(removeRec(root.getLeft(), key));
 		}
-		else if(key > root.getData()){
+		else if(key > root.getData()) {
+			// remove from right subtree
+			root.setRight(removeRec(root.getRight(), key));
+		}
+		else { //root == key
+			if(root.getFrequency()>1)
+				root.decreaseFrequency();
+			else {
+				//write the deletion code
+				// if we're at a leaf
+				if(root.getLeft()==null && root.getRight()==null)
+					root = null;
+				else if(root.getLeft()==null)// we only have the right child
+					return root.getRight();
+				else if(root.getRight()==null)// we only have the left child
+					return root.getLeft();
+				else {//we have two children
+					int min = findMin(root);
+					root.setData(min);
+					root.setRight(removeRec(root.getRight(), min));
+				}
+			}
+			count--;
+		}
+		
+		return root;
 
-			if(root.getRight()==null)
-				return false;
-			else {
-				MyBinarySearchTree current = new MyBinarySearchTree(root.getRight());
-				return current.search(key);
-			}
-		}
+	}
+	
+	public int findMin(MyNode root) {
+		if(root.getLeft()==null)
+			return root.getData();
+		else
+			return findMin(root.getLeft());
+	}
+	
+	public boolean search(int key) {
+		return searchRec(root, key);
+	}
+
+	private boolean searchRec(MyNode root, int key) {
+		if(root==null)
+			return false;
+		if(key < root.getData())//search left subtree
+				return searchRec(root.getLeft(),key);
+		else if(key > root.getData())//search right subtree
+			return searchRec(root.getRight(),key);
 		else // if key == data
 			return true;
 
 	}
-
+	
 	public void inorder() {
-		if(root.getLeft()!=null) {
-			MyBinarySearchTree current = new MyBinarySearchTree(root.getLeft());
-			current.inorder();
-		}
+		inorderRec(root);
+	}
+
+	private void inorderRec(MyNode root) {
+		if(root==null)
+			return;
+		inorderRec(root.getLeft());
+		
 		for(int i =0; i<root.getFrequency(); i++)
 			System.out.print(root.getData()+" ");
-		if(root.getRight()!=null) {
-			MyBinarySearchTree current = new MyBinarySearchTree(root.getRight());
-			current.inorder();
-
-		}
+		
+		inorderRec(root.getRight());
 	}
 
 	public int getHeight() {
 		return root.height();
 	}
 
+	public int getCount() {
+		return count;
+	}
 
 }
